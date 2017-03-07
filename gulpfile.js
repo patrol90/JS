cdfd7aea68a3c9de8f1153329e43d59779cd5352
Соболевский Дмитрий <dmitry.sobolevsky@patio-minsk.by>
2017-03-07 11:54:03 +0300

    watch

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,22 @@ sass = require('gulp-sass');
 browserSync = require('browser-sync'); // Подключаем Browser Sync
 
 gulp.task('sass', function(){ // Создаем таск "sass"
-    return gulp.src('DragDrop/*.sass') // Берем источник
+    return gulp.src('Clock/*.sass') // Берем источник
         .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
-        .pipe(gulp.dest('DragDrop/css')) // Выгружаем результата в папку app/css
+        .pipe(gulp.dest('Clock/css')) // Выгружаем результата в папку app/css
         .pipe(browserSync.reload({stream: true}))
 });
 gulp.task('watch', ['browser-sync', 'sass'], function() {
-    gulp.watch('DragDrop/*.sass', ['sass']); // Наблюдение за sass файлами
-    gulp.watch('DragDrop/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
-    gulp.watch('DragDrop/*.js', browserSync.reload); // Наблюдение за JS файлами в папке js
+    gulp.watch('Clock/*.sass', ['sass']); // Наблюдение за sass файлами
+    gulp.watch('Clock/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
+    gulp.watch('Clock/*.js', browserSync.reload); // Наблюдение за JS файлами в папке js
 
 });
 
 gulp.task('browser-sync', function() { // Создаем таск browser-sync
     browserSync({ // Выполняем browser Sync
         server: { // Определяем параметры сервера
-            baseDir: 'DragDrop' // Директория для сервера - app
+            baseDir: 'Clock' // Директория для сервера - app
         },
         notify: false // Отключаем уведомления
     });
