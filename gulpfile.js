9c88e21b4d38615702eb8299574863196d079afb
Соболевский Дмитрий <dmitry.sobolevsky@patio-minsk.by>
2017-03-13 17:53:24 +0300

    Tetris v0.1

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,22 @@ sass = require('gulp-sass');
 browserSync = require('browser-sync'); // Подключаем Browser Sync
 
 gulp.task('sass', function(){ // Создаем таск "sass"
-    return gulp.src('Clock/*.sass') // Берем источник
+    return gulp.src('TENNIS_DOM/*.sass') // Берем источник
         .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
-        .pipe(gulp.dest('Clock/css')) // Выгружаем результата в папку app/css
+        .pipe(gulp.dest('TENNIS_DOM/css')) // Выгружаем результата в папку app/css
         .pipe(browserSync.reload({stream: true}))
 });
 gulp.task('watch', ['browser-sync', 'sass'], function() {
-    gulp.watch('Clock/*.sass', ['sass']); // Наблюдение за sass файлами
-    gulp.watch('Clock/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
-    gulp.watch('Clock/*.js', browserSync.reload); // Наблюдение за JS файлами в папке js
+    gulp.watch('TENNIS_DOM/*.sass', ['sass']); // Наблюдение за sass файлами
+    gulp.watch('TENNIS_DOM/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
+    gulp.watch('TENNIS_DOM/*.js', browserSync.reload); // Наблюдение за JS файлами в папке js
 
 });
 
 gulp.task('browser-sync', function() { // Создаем таск browser-sync
     browserSync({ // Выполняем browser Sync
         server: { // Определяем параметры сервера
-            baseDir: 'Clock' // Директория для сервера - app
+            baseDir: 'TENNIS_DOM' // Директория для сервера - app
         },
         notify: false // Отключаем уведомления
     });
